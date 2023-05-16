56783750be4b14ac5707945191c336159abf99f6
Immanuel Balete <baletejosiah@gmail.com>
2023-05-16 11:33:56 +0800

    Update code to remove unneeded rows and columns

--- explore.py
+++ explore.py
@@ -3,4 +3,11 @@ import csv
 
 csv_path = 'data.csv'
 data = pd.read_csv(csv_path, sep=',')
-print(data.head())
+
+# Removing irrelevant rows and columns
+data = data.dropna(subset=['Timestamp']) # empty rows
+data = data.dropna(axis=1, how='all') # empty columns
+data = data.drop(axis=1, columns=['ID', 'Timestamp', 'Tweet URL', 'Group', 'Collector', 'Category', 'Topic', 'Screenshot', 'Reasoning', 'Remarks']) # columns with irrelevant data
+data = data.drop(axis=1, columns=['Quote Tweets', 'Views']) # columns which are mostly empty
+
+print(data)
