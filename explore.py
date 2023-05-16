d0d09d000f61628ca2e19cfba8355d485106bd3f
Immanuel Balete <baletejosiah@gmail.com>
2023-05-17 00:44:31 +0800

    Updated column formatting

--- explore.py
+++ explore.py
@@ -2,13 +2,23 @@ import pandas as pd
 import csv
 
 csv_path = 'data.csv'
-data = pd.read_csv(csv_path, sep=',')
+data = pd.read_csv(csv_path, sep=',', parse_dates = ['Joined', 'Date posted'])
 
 # Removing irrelevant rows and columns
 data = data.dropna(subset=['Timestamp']) # empty rows
 data = data.dropna(axis=1, how='all') # empty columns
 data = data.drop(axis=1, columns=['ID', 'Timestamp', 'Tweet URL', 'Group', 'Collector', 'Category', 'Topic', 'Screenshot', 'Reasoning', 'Remarks']) # columns with irrelevant data
 data = data.drop(axis=1, columns=['Quote Tweets', 'Views']) # columns which are mostly empty
+
+for col in data:
+  if col in ['Following', 'Followers', 'Likes', 'Replies', 'Retweets']:
+    data[col] = data[col].astype('int')
+  elif col!='Date posted':
+    data[col] = data[col].astype('category')
+print("Column types:")
+print(data.dtypes)
+print()
+
 
 print("Number of empty values per column:")
 print(data.isnull().sum())
@@ -23,3 +33,11 @@ print()
 tweet_type_list = [typ.lower() for value in data['Tweet Type'] for typ in value.split(',')]
 print("Number of each tweet type:")
 print(pd.DataFrame(tweet_type_list).value_counts())
+print()
+
+print("Number of each content type:")
+print(data['Content type'].value_counts())
+print()
+
+print("Numerical data statistics:")
+print(data.describe(datetime_is_numeric=True))
