21ea65014bf0245c3f47e8cf5c3c661caf4f943e
Immanuel Balete <baletejosiah@gmail.com>
2023-05-16 23:13:39 +0800

    Print keywords

--- explore.py
+++ explore.py
@@ -10,4 +10,11 @@ data = data.dropna(axis=1, how='all') # empty columns
 data = data.drop(axis=1, columns=['ID', 'Timestamp', 'Tweet URL', 'Group', 'Collector', 'Category', 'Topic', 'Screenshot', 'Reasoning', 'Remarks']) # columns with irrelevant data
 data = data.drop(axis=1, columns=['Quote Tweets', 'Views']) # columns which are mostly empty
 
-print(data)
+print("Number of empty values per column:")
+print(data.isnull().sum())
+print("\n")
+
+keywords_list = set([phrase.lower() for phrase in set(data['Keywords'])])
+keyword_set = sorted(set([word for phrase in keywords_list for word in phrase.split()]))
+print("Keywords used:")
+print(keyword_set)
